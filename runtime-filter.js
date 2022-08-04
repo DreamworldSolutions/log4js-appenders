9accd2b4b15edfa3d341a67ab0f43480bbc1d19f
Dhruval Kodinariya <dhruval@hisab.co>
2022-08-04 12:24:09 +0530

    feat: Implementation of runtime filter

--- runtime-filter.js
+++ runtime-filter.js
@@ -1,28 +1,36 @@
 /**
  * It filter's log events on runtime
+ * It filter's log events with default level, categoryFilterFn and contextFilterFn and pass to other appenders if any of
+ *  that returns true.
  * @param {Object} config
+ * @param {Function} categoryFilterFn
+ * @param {Function} contextFilterFn
  * @param {Array} appenders
  * @returns
  */
-function runtimeFilter(defaultLevel, appenders) {
+ function runtimeFilter(defaultLevel, categoryFilterFn, contextFilterFn, appenders) {
   return (logEvent) => {
-    if (logEvent["level"].isGreaterThanOrEqualTo(defaultLevel)) {
+    let { categoryName, level, context } = logEvent;
+
+    if (level.isGreaterThanOrEqualTo(defaultLevel) || 
+    categoryFilterFn(categoryName, level["levelStr"]) || 
+    contextFilterFn(context, level["levelStr"])) {
       appenders.forEach((appender) => {
         appender(logEvent);
       });
-      return;
     }
   };
 }
 
 function configure(config, layouts, findAppender) {
   let defaultLevel = config["default"] || "INFO";
-
+  let categoryFilterFn = config["category"] || function() { return false; };
+  let contextFilterFn = config["context"] || function() { return false; };
   let appenders = (config["appenders"] || []).map((appender) => {
     return findAppender(appender);
   });
 
-  return runtimeFilter(defaultLevel, appenders);
+  return runtimeFilter(defaultLevel, categoryFilterFn, contextFilterFn, appenders);
 }
 
 module.exports.configure = configure;
